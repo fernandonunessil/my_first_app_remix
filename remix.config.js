4a4db58ad7a1a713dd6aef1f3dbec0eeac5321a2
Fernando Nunes <fernandonunesjs@hotmail.com>
2024-01-13 22:41:14 -0300

    add and configure flat-routes

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,10 @@
+const { flatRoutes } = require("remix-flat-routes");
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  routes: async (defineRoutes) => {
+    return flatRoutes("routes", defineRoutes);
+  }
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
