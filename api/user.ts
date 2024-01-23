0347251f29f356906f8bdb46bb7b337a87f507bd
Fernando Nunes <fernandonunesjs@hotmail.com>
2024-01-23 05:36:10 -0300

    feat: users api add two routes

--- api/user.ts
+++ api/user.ts
@@ -1,12 +1,26 @@
+import { LoginParams } from "types/api/user/type";
 import { api } from "./api";
 
 export const getAllUsers = () => {
   return new Promise((resolve, reject) => {
     api
       .get("/getAllUsers")
-      .then((result) => {
-        resolve(result);
+      .then(({ data }) => {
+        resolve(data);
       })
       .catch((err) => {});
+  });
+};
+
+export const Login = (user: LoginParams) => {
+  return new Promise((resolve, reject) => {
+    api
+      .post("http://localhost:8080/login", user)
+      .then(({ data }) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 };
